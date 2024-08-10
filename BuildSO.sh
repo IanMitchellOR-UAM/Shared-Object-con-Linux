0e55ab96e65293487321e6ba2b2cd4967dbfdb51
Ian Mitchell Ochoa Román <al2203033249@azc.uam.mx>
2024-08-10 15:30:49 -0600

    Update BuildSO.sh

--- BuildSO.sh
+++ BuildSO.sh
@@ -1,16 +1,11 @@
-echo "hola_print_library.o: hola_print_library.c"
-gcc -c hola_print_library.c -o hola_print_library.o
+echo "hola_print_library.o: hola_print_library.c" echo "gcc -c hola_print_library.c -o hola_print_library.o"
 
-echo "lib_hola_print_library.so: hola_print_library.o"
-gcc -shared hola_print_library.o -o lib_hola_print_library.so
+echo "lib_hola_print_library.so: hola_print_library.o" echo "gcc -shared hola_print_library.o -o lib_hola_print_library.so"
 
-echo "holaSOexe: hola_main_library.c"
-gcc -L/home/usuario/so210/makeso/makelibso/ -Wall -o holaSOexe hola_main_library.c -l_hola_print_library
+echo "holaSOexe: hola_main_library.c" echo "gcc -L/home/usuario/Documents -Wall -o holaSOexe hola_main_library.c -l_hola_print_library"
 
 echo "to clean: rm hola_main_library.o hola_print_library.o lib_hola_print_library.so holaSOexe"
 
-echo "LD_LIBRARY_PATH=/home/usuario/so210/makeso/makelibso/:$LD_LIBRARY_PATH"
-LD_LIBRARY_PATH=/home/usuario/so210/makeso/makelibso/:$LD_LIBRARY_PATH
+echo "LD_LIBRARY_PATH=/home/usuario/Documents:$LD_LIBRARY_PATH" export LD_LIBRARY_PATH=/home/usuario/Documents:$LD_LIBRARY_PATH
 
-echo "Ejecucion con carga de la shared object"
-./holaSOexe
+echo "Ejecucion con carga de la shared object" ./holaSOexe
